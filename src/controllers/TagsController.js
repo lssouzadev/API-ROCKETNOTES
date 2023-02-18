79d2038bc0392e43f5f8b12a680e4df2953041a8
lssouzadev <leoss91@hotmail.com>
2023-02-18 17:52:16 -0300

    API ROCKET NOTES

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,14 @@
+/* eslint-disable camelcase */
+const knex = require('../database/knex')
+
+class TagsController {
+  async index(request, response) {
+    const user_id = request.user.id
+
+    const tags = await knex('tags').where({ user_id })
+
+    return response.json(tags)
+  }
+}
+
+module.exports = TagsController
